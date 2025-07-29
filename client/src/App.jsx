80293e65a39f0a10c8d9179984077b3891002027
Yedidya <didihazan123@gmail.com>
2025-07-29 09:30:33 +0300

    feat: add image modal component and additional info field; update participant schema and forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 import WeddingDatingForm from './components/WeddingDatingForm'
 import AdminDashboard from './components/AdminDashboard'
+import WebediaFooterLogo from "./components/WebediaFooterLogo.jsx";
 
 function App() {
     return (
-        <Routes>
-            {/* דף הטופס הראשי */}
-            <Route path="/" element={<WeddingDatingForm />} />
+        <div className="min-h-screen flex flex-col">
+            {/* תוכן הדפים */}
+            <div className="flex-1">
+                <Routes>
+                    <Route path="/" element={<WeddingDatingForm />} />
+                    <Route path="/wedding-admin-view-participants" element={<AdminDashboard />} />
+                    <Route path="*" element={<div className="min-h-screen flex items-center justify-center text-2xl">דף לא נמצא</div>} />
+                </Routes>
+            </div>
 
-            {/* דף התצוגה עם שם לא ברור */}
-            <Route path="/wedding-admin-view-participants" element={<AdminDashboard />} />
-
-            {/* דף 404 */}
-            <Route path="*" element={<div className="min-h-screen flex items-center justify-center text-2xl">דף לא נמצא</div>} />
-        </Routes>
+            {/* הפוטר - תמיד בתחתית */}
+            <WebediaFooterLogo />
+        </div>
     )
 }
 
